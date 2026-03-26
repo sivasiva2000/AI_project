0599b7dfd1afc863849085884702f7a7963a7e5c
siva k <144662906+sivasiva2000@users.noreply.github.com>
2026-03-26 17:51:26 +0530

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,36 @@
+from google.adk.agents import LlmAgent
+from google.adk.tools import google_search
+
+def total_count_of_identity_access_management_iga_agents() -> dict:
+    """Get the total count of identity access management (IGA) agents."""
+    return {
+        "total_identity": 100
+    }
+
+iga_assistance_agent = LlmAgent(
+name="iga_assistance_agent",
+model="gemini-2.5-flash",
+description="A simple IGA assistant ",
+instruction="""You are a friendly IGA assistant.
+You can help answer user's generic questions on IGA and help plan their IGA goals. Be more friendly and positive.""",
+
+tools=[total_count_of_identity_access_management_iga_agents]
+# tools=[google_search] # allow to use google search tool to answer user's question
+)
+
+
+root_agent = iga_assistance_agent
+
+
+
+# finance_assistance_agent = LlmAgent(
+# name="finance_assistance_agent",
+# model="gemini-2.5-flash",
+# description="A simple finance assistant that helps with user's finance goals.",
+# instruction="""You are a friendly finance assistant.
+# You can help answer user's generic questions on finance and help plan
+# their finance goals. Be more friendly and positive."""
+# )
+
+
+# root_agent = finance_assistance_agent
